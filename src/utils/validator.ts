27d60a75b9671cb643920385c21e4ad652f20cc2
Bogdan <b@bmazur.ru>
2022-08-15 18:43:29 +0300

    feat: validation

---
+++ src/utils/validator.ts
@@ -0,0 +1,36 @@
+import React, { useCallback } from 'react';
+
+function useFormWithValidation() {
+  const [values, setValues] = React.useState({});
+  const [errors, setErrors] = React.useState({});
+  const [isValid, setIsValid] = React.useState(false);
+
+  const handleChange = (event: { target: any; }) => {
+    const { target } = event;
+    const { name } = target;
+    const { value } = target;
+
+    setValues({ ...values, [name]: value });
+    setErrors({ ...errors, [name]: target.validationMessage });
+    setIsValid(target.closest('form').checkValidity());
+  };
+
+  const resetForm = useCallback(
+    (
+      newValues: object = {},
+      newErrors: object = {},
+      newIsValid = false,
+    ) => {
+      setValues(newValues);
+      setErrors(newErrors);
+      setIsValid(newIsValid);
+    },
+    [setValues, setErrors, setIsValid],
+  );
+
+  return {
+    values, handleChange, errors, isValid, resetForm, setIsValid, setValues,
+  };
+}
+
+export default useFormWithValidation;
