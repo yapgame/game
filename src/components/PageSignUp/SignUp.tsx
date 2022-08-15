c87aee7efb0c4b329c3d9b6015e88387f4a5bf33
Bogdan <b@bmazur.ru>
2022-08-15 18:53:41 +0300

    feat: SignUp

---
+++ src/components/PageSignUp/SignUp.tsx
@@ -0,0 +1,109 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { NavLink } from 'react-router-dom';
+import { SIGNIN_URL } from '../../utils/constants';
+import useFormWithValidation from '../../utils/validator';
+
+import { IProps } from './IProps';
+import { IValid } from './IValid';
+
+function SignUp(props: IProps) {
+  const {
+    values,
+    errors,
+    isValid,
+    handleChange,
+  }: IValid = useFormWithValidation();
+  const { handleSignUp } = props;
+  const handleSubmit = (evt: any) => {
+    evt.preventDefault();
+    handleSignUp(values);
+  };
+  return (
+    <Container maxWidth="lg">
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        gridColumn="span 1"
+        sx={{
+          '& .MuiTextField-root': { m: 2 },
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          textAlign: 'center',
+          alignItems: 'center',
+          minHeight: 800,
+        }}
+      >
+        <Typography variant="h5" gutterBottom component="div">
+          SignUp
+        </Typography>
+        <TextField
+          error={!!errors.email}
+          sx={{
+            m: 2,
+            maxWidth: '40ch',
+            width: '100%',
+          }}
+          required
+          id="outlined-required"
+          label="E-mail"
+          defaultValue=""
+          name="email"
+          onChange={handleChange}
+          helperText={errors.email}
+          type="email"
+        />
+        <TextField
+          error={!!errors.password}
+          sx={{
+            m: 2,
+            maxWidth: '40ch',
+            width: '100%',
+          }}
+          required
+          id="outlined-required"
+          label="Password"
+          defaultValue=""
+          name="password"
+          onChange={handleChange}
+          helperText={errors.password}
+          type="password"
+          autoComplete="current-password"
+          inputProps={{
+            minLength: 6,
+          }}
+        />
+        <Button
+          variant="outlined"
+          size="large"
+          sx={{
+            m: 4,
+            maxWidth: '43ch',
+            width: '100%',
+          }}
+          type="submit"
+          disabled={!isValid}
+        >
+          SignUp
+        </Button>
+        <NavLink
+          to={SIGNIN_URL}
+          style={{
+            margin: '0',
+            color: '#1976d2',
+            textDecoration: 'none',
+          }}
+        >
+          SignIn
+        </NavLink>
+      </Box>
+    </Container>
+  );
+}
+
+export default SignUp;
